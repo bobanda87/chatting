6157624c9cde6db06ee4753c92edf869287387c9
Boban Davidovic <boban.da87@gmail.com>
2015-12-09 02:49:25 +0100

    Initial commit

---
+++ Chatting/Message.swift
@@ -0,0 +1,43 @@
+//
+//  Message.swift
+//  Chatting
+//
+//  Created by Boban Davidovic on 12/9/15.
+//  Copyright © 2015 Boban Davidovic. All rights reserved.
+//
+
+import Foundation
+
+class Message : NSObject, JSQMessageData {
+    var text_: String
+    var sender_: String
+    var date_: NSDate
+    var imageUrl_: String?
+    
+    convenience init(text: String?, sender: String?) {
+        self.init(text: text, sender: sender, imageUrl: nil)
+    }
+    
+    init(text: String?, sender: String?, imageUrl: String?) {
+        self.text_ = text!
+        self.sender_ = sender!
+        self.date_ = NSDate()
+        self.imageUrl_ = imageUrl
+    }
+    
+    func text() -> String! {
+        return text_;
+    }
+    
+    func sender() -> String! {
+        return sender_;
+    }
+    
+    func date() -> NSDate! {
+        return date_;
+    }
+    
+    func imageUrl() -> String? {
+        return imageUrl_;
+    }
+}
